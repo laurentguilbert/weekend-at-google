9b59dc62ffc1d0ed3ddd308106eabebb54ec3f67
badaud_t <thomas@badaud.fr>
2014-04-05 13:13:06 +0200

    wk progress Graph

--- graph.py
+++ graph.py
@@ -1,28 +1,31 @@
 import networkx as nx
 
+import baseobjects
+
 class Graph(object):
-    def get_edge_dict(self, e):
-        return self.dg[e[0]][e[1]]
+    def edge_dict(self, e):
+        return self.g[e[0]][e[1]]
+    def edge_attr(self, e, key):
+        return self.g[e[0]][e[1]][key]
 
-    def get_edge_attr(self, e, key):
-        self.dg[e[0]][e[1]][key] = val
-
+    # getter shortcuts
+    def edge_cost(self, e):
+        return self.edge_attr('cost')
     def edge_len(self, e):
-        return self.get_edge_attr('len')
+        return self.edge_attr('len')
     def edge_score(self, e):
-        return self.get_edge_attr('score')
+        return self.edge_attr('score')
     def edge_is_visited(self, e):
-        return self.get_edge_attr('visited')
+        return self.edge_attr('visited')
 
     def set_edge_attr(self, e, key, val):
-        self.dg[e[0]][e[1]][key] = val
-
-    def edge_set_visited(self, e, *args, **kwargs):
+        self.g[e[0]][e[1]][key] = val
+    def set_edge_visited(self, e, *args, **kwargs):
         self.set_edge_attr('visited', True)
 
     def cmp_edge_score(self, e, e1, *args, **kwargs):
         """ e[score] > e1[score] """
-        return self.get_edge_attr(e, 'score') > self.get_edge_attr(e1, 'score')
+        return self.edge_attr(e, 'score') > self.edge_attr(e1, 'score')
 
     def __init__(self, atchoum):
         self.atchoum = atchoum
@@ -34,13 +37,16 @@ class Graph(object):
 
         for street in atchoum.streets:
             attr_dict = {
-                'weight': street.cost,
+                'cost': street.cost,
                 'len': street.len,
                 'score': street.score,
                 'visited': False,
             }
             dg.add_edge(street.i1, street.i2, **attr_dict)
-            if street.way == 2:
+            if street.way == baseobjects.WAY.DOUBLE:
                 dg.add_edge(street.i2, street.i1, **attr_dict)
 
-        self.dg = dg
+        self.g = dg
+
+    def edges(self, start_node):
+        return self.g.edges(start_node)
