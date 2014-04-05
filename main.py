f7d484a2823742a7e105b9379cfed6062e814136
Laurent Guilbert <laurent.guilbert91@gmail.com>
2014-04-05 12:17:40 +0200

    New structure with graph parsing.

--- main.py
+++ main.py
@@ -1,42 +1,23 @@
-import os
+# flake8: noqa
+import networkx as nx
 
+from baseobjects import AtchoumParser
 
-class Intersection(object):
-    def __init__(self, c1, c2):
-        self.c1 = float(c1)
-        self.c2 = float(c2)
-
-class Street(object):
-    def __init__(self, inter1, inter2, way, cost, len):
-        self.i1 = int(inter1)
-        self.i2 = int(inter2)
-        self.way = int(way)
-        self.cost = int(cost)
-        self.len = int(len)
-
-def to_inter_object(inters):
-    return [Intersection(*inter.split()) for inter in inters]
-
-def to_street_object(streets):
-    return [Street(*street.split()) for street in streets]
 
 def main(filename="paris_54000.txt"):
-    lines = []
-    with open(filename) as f:
-        lines = f.readlines()
-    print lines[0]
+    data = AtchoumParser.from_filename(filename)
+    inters_obj = data.inters
+    streets_obj = data.streets
 
-    (n, m, t, c, s, ) = lines[0].split()
+    # generate graph
+    dg = nx.DiGraph()
 
-    inter = lines[1:int(n)+1]
-    streets = lines[int(n)+1:]
-    print inter[0], inter[-1], len(inter)
-    print streets[0], streets[-1], len(streets)
+    for idx, inter in enumerate(inters_obj):
+        dg.add_node(idx, inter.__dict__)
 
-    intersobj = to_inter_object(inter)
-    streetsobj = to_street_object(streets)
-
-    print intersobj[0].__dict__
-    print streetsobj[0].__dict__
+    for street in streets_obj:
+        dg.add_edge(street.i1, street.i2, weight=street.cost, attr_dict=street.__dict__)
+        if street.way == 2:
+            dg.add_edge(street.i2, street.i1, weight=street.cost, attr_dict=street.__dict__)
 
 main()
--- routage_street_view.py
+++
@@ -1,6 +0,0 @@
-import networkx as nx
-
-def main():
-    g = nx.DiGraph()
-
-main()
