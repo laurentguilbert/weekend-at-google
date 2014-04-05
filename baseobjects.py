ddda7d68d0f6ea4f00605b026fb3d4f667dd9252
badaud_t <thomas@badaud.fr>
2014-04-05 12:05:44 +0200

    + atchoumparser

---
+++ baseobjects.py
@@ -0,0 +1,53 @@
+from helpers import enum
+
+WAY = enum(UNIQUE=1, DOUBLE=2)
+
+class Intersection(object):
+    def __init__(self, c1, c2):
+        self.c1 = float(c1)
+        self.c2 = float(c2)
+
+class Street(object):
+    def __init__(self, inter1, inter2, way, cost, len):
+        self.i1 = int(inter1)
+        self.i2 = int(inter2)
+        self.way = int(way)
+        self.cost = int(cost)
+        self.len = int(len)
+
+class AtchoumParser(object):
+
+    def __init__(self, inters, streets, time, cars, start):
+        self.inters = inters
+        self.streets = streets
+        self.time = int(time)
+        self.cars = int(cars)
+        self.start = int(start)
+
+    @staticmethod
+    def from_filename(filename):
+        def to_inter_object(inters):
+            return [Intersection(*inter.split()) for inter in inters]
+
+        def to_street_object(streets):
+            return [Street(*street.split()) for street in streets]
+
+        lines = []
+        with open(filename) as f:
+            lines = f.readlines()
+        print lines[0]
+
+        (n, m, time, cars, start, ) = lines[0].split()
+
+        n = int(n)
+        m = int(m)
+        inters = lines[1:n+1]
+        streets = lines[n+1:]
+        if len(inters) != n: raise Exception("fock 1")
+        if len(streets) != m: raise Exception("fock 2")
+
+        inters = to_inter_object(inters)
+        streets = to_street_object(streets)
+
+        return AtchoumParser(inters, streets, time, cars, start)
+        self.inters
