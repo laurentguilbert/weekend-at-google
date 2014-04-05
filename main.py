e5da875cf7e9c8ee5ec4a52180ef8ba314c6d615
Laurent Guilbert <laurent.guilbert91@gmail.com>
2014-04-05 12:26:55 +0200

    Plop.

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import networkx as nx
 from baseobjects import AtchoumParser
 
 
-def main(filename="paris_54000.txt"):
+def main(filename="example.txt"):
     data = AtchoumParser.from_filename(filename)
     inters_obj = data.inters
     streets_obj = data.streets
+
+    cars_count = data.cars
+    max_cost = data.time
+    start_node = data.start
+
+    print "cars count: {}".format(cars_count)
+    print "max cost: {}".format(max_cost)
+    print "start node: {}".format(start_node)
 
     # generate graph
     dg = nx.DiGraph()
@@ -16,8 +24,10 @@ def main(filename="paris_54000.txt"):
         dg.add_node(idx, inter.__dict__)
 
     for street in streets_obj:
-        dg.add_edge(street.i1, street.i2, weight=street.cost, attr_dict=street.__dict__)
+        dg.add_edge(street.i1, street.i2, weight=street.cost, len=street.len)
         if street.way == 2:
-            dg.add_edge(street.i2, street.i1, weight=street.cost, attr_dict=street.__dict__)
+            dg.add_edge(street.i2, street.i1, weight=street.cost, len=street.len)
+
+    print dg[start_node]
 
 main()
